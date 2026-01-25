9cfaa4c8cdb9a1fd03ff56bfe7ac1290d8d48903
prafdin <pravdin.biss@gmail.com>
2026-01-25 19:40:25 +0400

    Add tests

---
+++ app/test_app.py
@@ -0,0 +1,13 @@
+import pytest
+from app.app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_index(client):
+    """Test that the index page returns a 200 OK status code."""
+    response = client.get('/')
+    assert response.status_code == 200
